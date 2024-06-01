3ff0784b61e9ee56ec70ccb65f3db5b8142d9385
Katsu <messyaikei456@gmail.com>
2024-06-01 17:30:08 +0530

    update

--- Metro_app/Metro_app/settings.py
+++ Metro_app/Metro_app/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'Metro_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metro',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'shreyosee123',
+
+
     }
 }
 
@@ -117,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'src'),)
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
